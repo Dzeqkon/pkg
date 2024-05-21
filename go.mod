30c6c349dc466afb76eabef845e2262c03b8fea7
azh <2321688645@qq.com>
2024-05-21 17:12:07 +0800

    perf:tools

--- go.mod
+++ go.mod
@@ -8,12 +8,15 @@ require (
 )
 
 require (
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
-	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mr-tron/base58 v1.2.0
-	golang.org/x/crypto v0.19.0 // indirect
+	github.com/sony/sonyflake v1.2.0
+	github.com/speps/go-hashids v2.0.0+incompatible
+	golang.org/x/crypto v0.19.0
 	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
--- strings/string.go
+++ strings/string.go
@@ -3,7 +3,9 @@ package strings
 import (
 	"strconv"
 	"strings"
+	"unicode/utf8"
 
+	"github.com/asaskevich/govalidator"
 	"github.com/dzeqkon/pkg/symbol"
 )
 
@@ -264,9 +266,65 @@ func FirstCaseToUpper(str string, first bool) string {
 	}
 }
 
+func CamelCaseToUnderscore(str string) string {
+	return govalidator.CamelCaseToUnderscore(str)
+}
+
+func UnderscoreToCamelCase(str string) string {
+	return govalidator.UnderscoreToCamelCase(str)
+}
+
+func Reverse(s string) string {
+	size := len(s)
+	buf := make([]byte, size)
+	for start := 0; start < size; {
+		r, n := utf8.DecodeRuneInString(s[start:])
+		start += n
+		utf8.EncodeRune(buf[size-start:], r)
+	}
+	return string(buf)
+}
+
 /*
 [9 9 8 4 2 9 1 7 - a 5 4 b - 3 3 1 6 - c d f 3 - 8 7 d 9 f b 5 7] -> "99842917-a54b-3316-cdf3-87d9fb57"
 */
 func ArrayToString(arrays []string) string {
 	return strings.Join(arrays, "")
+}
+
+func Diff(base, exclude []string) (result []string) {
+	excludeMap := make(map[string]bool)
+	for _, s := range exclude {
+		excludeMap[s] = true
+	}
+	for _, s := range base {
+		if !excludeMap[s] {
+			result = append(result, s)
+		}
+	}
+	return result
+}
+
+func Unique(ss []string) (result []string) {
+	smap := make(map[string]bool)
+	for _, s := range ss {
+		smap[s] = true
+	}
+	for s := range smap {
+		result = append(result, s)
+	}
+	return result
+}
+
+func FindString(array []string, str string) int {
+	for index, s := range array {
+		if str == s {
+			return index
+		}
+	}
+	return -1
+}
+
+func StringIn(str string, array []string) bool {
+	return FindString(array, str) > -1
 }
